bcad654d89d80830f36be7fe48599abc580558f6
Ricardo Soares Ribolli <rsoaresribolli@gmail.com>
2022-01-25 16:39:45 -0300

    feat: mongodb helper

---
+++ src/external/repositories/mongodb/helper/mongo-helper.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+  async disconnect (): Promise<void> {
+    this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.clienc.db().collection(name)
+  },
+  clearCollection (name: string): void {
+    this.client.db().collection(name).deleteMany({})
+  }
+
+}
