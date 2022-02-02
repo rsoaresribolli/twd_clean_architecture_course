d2d06844c52d3b8555cdc8a0afd39a275f7cd245
Ricardo Soares Ribolli <rsoaresribolli@gmail.com>
2022-02-01 22:32:49 -0300

    fix: make usecase return userData

--- src/usecases/register-and-send-email/register-and-send-email.ts
+++ src/usecases/register-and-send-email/register-and-send-email.ts
@@ -15,7 +15,7 @@ export class RegisterAndSendEmail implements UseCase {
     this.sendEmail = sendEmail
   }
 
-  async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, User>> {
+  async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, UserData>> {
     const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
     if (userOrError.isLeft()) {
       return left(userOrError.value)
@@ -29,6 +29,6 @@ export class RegisterAndSendEmail implements UseCase {
       return left(result.value)
     }
 
-    return right(user)
+    return right({ name: user.name.value, email: user.email.value })
   }
 }
