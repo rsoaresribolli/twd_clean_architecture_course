3cbb0c42c92551fd6f593380fec9fd16d32b6d78
Ricardo Soares Ribolli <rsoaresribolli@gmail.com>
2022-02-03 20:22:24 -0300

    feat: use register and send email use case

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,10 +1,17 @@
 import { RegisterAndSendEmailController } from '@/web-controllers'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { MongodbUserRepository } from '@/external/repositories/mongodb'
+import { SendEmail } from '@/usecases/send-email'
+import { NodemailerEmailService } from '@/external/mailservices'
+import { getEmailOptions } from '@/main/config/email'
+import { RegisterAndSendEmail } from '@/usecases/register-and-send-email'
 
-export const makeRegisterUserController = (): RegisterAndSendEmailController => {
+export const makeRegisterAndSendEmailController = (): RegisterAndSendEmailController => {
   const mongodbUserRepository = new MongodbUserRepository()
   const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongodbUserRepository)
-  const registerUserController = new RegisterAndSendEmailController(registerUserOnMailingListUseCase)
+  const emailService = new NodemailerEmailService()
+  const sendEmailUseCase = new SendEmail(getEmailOptions(), emailService)
+  const registerAndSendMailUseCase = new RegisterAndSendEmail(registerUserOnMailingListUseCase, sendEmailUseCase)
+  const registerUserController = new RegisterAndSendEmailController(registerAndSendMailUseCase)
   return registerUserController
 }
